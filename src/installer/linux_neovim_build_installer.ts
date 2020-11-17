0a3a43791390454a0e55baf8635c2a612948b3d1
thinca <thinca@gmail.com>
2020-11-18 01:43:02 +0900

    Add a trick for building old Neovim
    Does not add tests for these.
    Cannot guarantee that it will work.

--- src/installer/linux_neovim_build_installer.ts
+++ src/installer/linux_neovim_build_installer.ts
@@ -2,19 +2,41 @@ import {exec} from "@actions/exec";
 import {gitClone} from "../commands";
 import {FixedVersion} from "../interfaces";
 import {NeovimBuildInstaller} from "./neovim_build_installer";
+import * as path from "path";
+import {readFileSync} from "fs";
 
 export class LinuxNeovimBuildInstaller extends NeovimBuildInstaller {
   async install(vimVersion: FixedVersion): Promise<void> {
-    // WIP
     const reposPath = this.repositoryPath();
-    await exec("sudo", ["apt-get", "update"]);
+    await gitClone("neovim/neovim", vimVersion, reposPath);
+
     const packages = [
       "ninja-build", "gettext", "libtool", "libtool-bin",
       "autoconf", "automake", "cmake", "g++", "pkg-config", "unzip",
     ];
+    const configureArgs = [
+      "CMAKE_BUILD_TYPE=RelWithDebInfo",
+      `CMAKE_EXTRA_FLAGS=-DCMAKE_INSTALL_PREFIX=${this.installDir}`,
+    ];
+
+    // XXX: Trick for v0.2.x.  This condition is not strict.
+    if (this.findLine(path.join(reposPath, "third-party/cmake/BuildLuarocks.cmake"), "luacheck-scm-1.rockspec")) {
+      packages.push("libuv1-dev", "libmsgpack-dev", "libtermkey-dev", "lua5.2", "lua-lpeg", "lua-mpack", "lua-bitop", "libluajit-5.1-dev", "gperf");
+      configureArgs.push("DEPS_CMAKE_FLAGS=-DUSE_BUNDLED=OFF -DUSE_BUNDLED_LIBVTERM=ON -DUSE_BUNDLED_UNIBILIUM=ON");
+    }
+
+    await exec("sudo", ["apt-get", "update"]);
     await exec("sudo", ["apt-get", "install", ...packages]);
-    await gitClone("neovim/neovim", vimVersion, reposPath);
-    await exec("make", ["CMAKE_BUILD_TYPE=RelWithDebInfo", `CMAKE_EXTRA_FLAGS=-DCMAKE_INSTALL_PREFIX=${this.installDir}`], {cwd: reposPath});
+    await exec("make", configureArgs, {cwd: reposPath});
     await exec("make", ["install"], {cwd: reposPath});
+  }
+
+  findLine(filepath: string, target: string): boolean {
+    try {
+      const content = readFileSync(filepath, {encoding: "utf8"});
+      return 0 <= content.indexOf(target);
+    } catch (e) {
+      return false;
+    }
   }
 }
