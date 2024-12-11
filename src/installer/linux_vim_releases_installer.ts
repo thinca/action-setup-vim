6419c3f0b7be795b6d5daa4d15407077d2af539e
thinca <thinca@gmail.com>
2024-12-12 00:20:00 +0900

    Install libglib2.0-dev for ubuntu 24.04

--- src/installer/linux_vim_releases_installer.ts
+++ src/installer/linux_vim_releases_installer.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
+import * as semver from "semver";
 import * as core from "@actions/core";
 import {exec} from "@actions/exec";
 import * as io from "@actions/io";
@@ -16,10 +17,22 @@ export class LinuxVimReleasesInstaller extends SemverReleasesInstaller {
   }
 
   async install(vimVersion: FixedVersion): Promise<void> {
-    // libfuse2 and LD_PRELOAD are needed for appimage on Ubuntu 22.04.
+    const ubuntuVersion = getUbuntuVersion();
+    // libfuse2 is needed for appimage.
+    const packages = ["libfuse2"];
+    if (ubuntuVersion === "22.04") {
+    // LD_PRELOAD are needed for appimage on Ubuntu 22.04.
+      core.exportVariable("LD_PRELOAD", "/lib/x86_64-linux-gnu/libgmodule-2.0.so");
+    }
+    if (ubuntuVersion === "24.04") {
+      const vimSemver = semver.coerce(vimVersion, {loose: true});
+      if (vimSemver && semver.lt(vimSemver, "8.2.5114", true)) {
+        packages.push("libglib2.0-dev");
+        core.exportVariable("LD_PRELOAD", "/lib/x86_64-linux-gnu/libgmodule-2.0.so");
+      }
+    }
     await exec("sudo", ["apt-get", "update"]);
-    await exec("sudo", ["apt-get", "install", "libfuse2"]);
-    core.exportVariable("LD_PRELOAD", "/lib/x86_64-linux-gnu/libgmodule-2.0.so");
+    await exec("sudo", ["apt-get", "install", ...packages]);
 
     const archiveFilePath = await this.downloadAsset(vimVersion);
     const installPath = this.getPath();
@@ -32,4 +45,10 @@ export class LinuxVimReleasesInstaller extends SemverReleasesInstaller {
   getPath(): string {
     return path.join(this.installDir, "bin");
   }
+}
+
+function getUbuntuVersion(): string {
+  const version = fs.readFileSync("/etc/os-release", "utf8");
+  const matched = /VERSION_ID="(\d+\.\d+)"/.exec(version);
+  return matched ? matched[1] : "";
 }
